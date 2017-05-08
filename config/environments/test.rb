9b1e2b53c39b906a4e0ef6e462b298180c4f2bcf
Keaton Burleson <keaton.burleson@me.com>
2017-05-08 11:37:06 -0500

    CSS bugs fixed, much better UI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,11 @@ Rails.application.configure do
     'Cache-Control' => 'public, max-age=3600'
   }
 
+  config.assets.precompile += %w( '.svg' )  
+
+  # Must include to get inline SVGs to work in deploy
+  config.assets.css_compressor = :sass
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
