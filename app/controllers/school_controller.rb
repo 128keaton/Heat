35708efbbd3b3262d6aeffc30eb406b4531153f1
Keaton Burleson <keaton.burleson@me.com>
2017-05-09 11:33:58 -0500

    DId a pretty

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,44 +1,44 @@
 class SchoolController < ApplicationController
-  def index
-    @machine = Machine.new
-    @schools = School.all
-    @roles = Role.all
-    if flash[:notice]
-      @type = params[:type]
-    end
-    if flash[:school]
-      @school = flash[:school]
-    end
-  end
+	def index
+		@machine = Machine.new
+		@schools = School.all
+		@roles = Role.all
+		if flash[:notice]
+			@type = params[:type]
+		end
+		if flash[:school]
+			@school = flash[:school]
+		end
+	end
 
-  def get_quantity_for(location, role)
-    @machineArray = Machine.where(location: location, role: role)
-    return @machineArray.length
-  end
+	def get_quantity_for(location, role)
+		@machineArray = Machine.where(location: location, role: role)
+		return @machineArray.length
+	end
 
-  def load_schools
-    @school = params[:machine][:location]
-    flash[:school] = @school
-    redirect_to controller: 'school', action: 'index'
-  end
+	def load_schools
+		@school = params[:machine][:location]
+		flash[:school] = @school
+		redirect_to controller: 'school', action: 'index'
+	end
 
-  def assign
-    if Machine.where(serial_number: params[:machine][:serial_number]).length != 0
-      existingMachine = Machine.where(serial_number: params[:machine][:serial_number])
-      existingMachine.update(location: params[:machine][:location])
-      flash[:notice] = "Machine was assigned"
-      redirect_to controller: 'school', action: 'index', type: "success"
-    else
-      flash[:notice] = "Serial number was not added beforehand"
-      redirect_to controller: 'school', action: 'index', type: "error"
-    end
-  end
+	def assign
+		if Machine.where(serial_number: params[:machine][:serial_number]).length != 0
+			existingMachine = Machine.where(serial_number: params[:machine][:serial_number])
+			existingMachine.update(location: params[:machine][:location])
+			flash[:notice] = "Machine was assigned"
+			redirect_to controller: 'school', action: 'index', type: "success"
+		else
+			flash[:notice] = "Serial number was not added beforehand"
+			redirect_to controller: 'school', action: 'index', type: "error"
+		end
+	end
 
-  helper_method :get_quantity_for
+	helper_method :get_quantity_for
 
-  private
+	private
 
-  def machine_params
-    params.require(:machine).permit(:location, :serial_number)
-  end
+	def machine_params
+		params.require(:machine).permit(:location, :serial_number)
+	end
 end
