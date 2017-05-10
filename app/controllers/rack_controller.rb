431f27e5e47b9ac875db14d4748fbe8ee044a743
Keaton Burleson <keaton.burleson@me.com>
2017-05-10 13:32:45 -0500

    Some minor UI tweaks, added a notation area too

--- app/controllers/rack_controller.rb
+++ app/controllers/rack_controller.rb
@@ -5,9 +5,10 @@ class RackController < ApplicationController
   end
 
   def assign
+		@user = current_user
     if Machine.where(serial_number: params[:machine][:serial_number]).length != 0
 			existingMachine = Machine.where(serial_number: params[:machine][:serial_number])
-			existingMachine.update(client_asset_tag: params[:machine][:client_asset_tag], reviveit_asset_tag: params[:machine][:reviveit_asset_tag], rack: params[:machine][:rack], racked: {"date" => Time.now.strftime("%d/%m/%Y %H:%M")})
+			existingMachine.update(client_asset_tag: params[:machine][:client_asset_tag], reviveit_asset_tag: params[:machine][:reviveit_asset_tag], rack: params[:machine][:rack], racked: {"date" => Time.now.strftime("%d/%m/%Y %H:%M"), "user" => @user.name})
 			flash[:notice] = "Machine was assigned a rack"
 			redirect_to controller: 'rack', action: 'index', type: "success"
 		else
