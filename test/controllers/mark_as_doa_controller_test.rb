2b5099df25d67833a812df351ec5f2cf38bbd07a
Keaton Burleson <keaton.burleson@me.com>
2017-05-19 15:23:10 -0500

    Remove a bad test

--- test/controllers/mark_as_doa_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MarkAsDoaControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get mark_as_doa_index_url
-    assert_response :success
-  end
-
-end
