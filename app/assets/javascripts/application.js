54647baac97b78bb10a859c336dbb581628890ec
Keaton Burleson <keaton.burleson@me.com>
2017-05-19 14:47:51 -0500

    Closes #3 - bastard!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         $("#audio-error").trigger('play')
         navigator.vibrate([500]);
         $('#notice\\ error').animate({
-            height: '120'
+            height: '80'
         })
         $('html,body').animate({
             scrollTop: $('#notice\\ error').offset().top
