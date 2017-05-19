e9860faf64c7af48d4700d11ba97f9838f85fd1d
Keaton Burleson <keaton.burleson@me.com>
2017-05-19 10:16:37 -0500

    Closes #9 - Now we assign the asset tag during unboxing

--- app/assets/javascripts/school.js
+++ app/assets/javascripts/school.js
@@ -1,11 +1,33 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#submit').attr('disabled', 'disabled');
-    $('#machine_serial_number').bind('input', function () {
-        if ($('#machine_serial_number').val().length === 7) {
+    $('#machine_serial_number').bind('input', function() {
+        if ($('#machine_serial_number').val().length === 7 && $('#machine_client_asset_tag').val()) {
+            return $('#submit').removeAttr('disabled');
+        } else {
+            return $('#submit').attr('disabled', 'disabled');
+        }
+    });
+    $('#machine_client_asset_tag').bind('input', function() {
+        if ($('#machine_serial_number').val().length === 7 && $('#machine_client_asset_tag').val()) {
             return $('#submit').removeAttr('disabled');
         } else {
             return $('#submit').attr('disabled', 'disabled');
         }
     });
     $("#machine_serial_number").focus();
+
+        $('.text-input').keypress(function (event) {
+        var currentBoxNumber, nextBox, textboxes;
+        if (event.keyCode === 13) {
+            textboxes = $('.text-input');
+            currentBoxNumber = textboxes.index(this);
+            if (textboxes[currentBoxNumber + 1] !== null) {
+                nextBox = textboxes[currentBoxNumber + 1];
+                nextBox.focus();
+                nextBox.select();
+                event.preventDefault();
+                return false;
+            }
+        }
+    });
 });
