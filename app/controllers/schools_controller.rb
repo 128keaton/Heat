94815988882c5029ab562f2f78e782db4eb1f4bf
Keaton Burleson <keaton.burleson@me.com>
2017-05-22 13:07:42 -0500

    Minor refactor

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -14,39 +14,45 @@ class SchoolsController < ApplicationController
   end
 
  def destroy
-    @school = School.find(params[:id])
-    @school.destroy
-    flash[:notice] = "School deleted successfully"
-    flash[:type] = "success"
-    redirect_to action: 'index'
+   # time to blow up the school :D
+   @school = School.find(params[:id])
+   @school.destroy
+   set_flash('School deleted successfully', 'success')
+   redirect_to action: 'index'
+  end
+
+  # Reusable method to set those lovely flash messages
+  def set_flash(notice, type = 'success')
+    flash[:notice] = notice
+    flash[:type] = type
   end
 
   def update
-     @school = School.where(school_code: params[:school][:school_code])[0]
+    find_school
+    return if @school.nil?
+    @school.quantity = update_quantity(@school)
+    if @school.valid?
+       @school.save
+      set_flash('School updated successfully')
+    else
+      set_flash('School could not be updated', 'error')
+    end
+    redirect_to action: 'index'
+  end
+  
+  # Try to locate school first by code, then by name
+  def find_school
+    @school = School.where(school_code: params[:school][:school_code])[0]
      if @school == nil
         @school = School.where(name: params[:name])[0]
      end
-     if params[:school][:school_code]
-      @school.school_code = params[:school][:school_code]
+     school_code(@school)
+  end
+
+  def school_code(school)
+      if params[:school][:school_code]
+        school.school_code = params[:school][:school_code]
      end
-
-
-     quantities = {}
-     Role.all.each do |role|
-      quantities[role.name] = params["#{role.name}-role"]
-     end
-
-     @school.quantity = quantities
-    if @school.valid?
-       @school.save
-       flash[:notice] = "School updated successfully"
-       flash[:type] = "success"
-       redirect_to action: 'index'
-    else
-      flash[:notice] = "School could not be updated"
-      flash[:type] = "error"
-      redirect_to action: 'index'
-    end
   end
 
   def edit
@@ -58,6 +64,14 @@ class SchoolsController < ApplicationController
     if flash[:notice]
       @type = params[:type]
     end
+  end
+
+  def update_quantity(school)
+    quantities = {}
+    Role.all.each do |role|
+      quantities[role.name] = params["#{role.name}-role"]
+    end
+    quantities
   end
 
   def validate(school)
