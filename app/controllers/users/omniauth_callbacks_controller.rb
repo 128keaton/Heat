83e1ae8abea2af8cbe629b6fb63ea185f04fce12
Keaton Burleson <keaton.burleson@me.com>
2017-05-22 14:37:44 -0500

    Sessions related refactor

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    @user = request_from_env
     if @user.email.split("@").last == "er2.com"
-        supervisor_list = Rails.configuration.api["supervisors"]
-        admin_list = Rails.configuration.api["admins"]
-
-        if admin_list.include? @user.email
-            @user.update_attribute :admin, true
-            @user.update_attribute :supervisor, true
-            @user.save
-        elsif supervisor_list.include? @user.email
-            @user.update_attribute :supervisor, true
-            @user.save
-        else
-            @user.update_attribute :admin, false
-            @user.update_attribute :supervisor, false
-            @user.save
-        end
-        
+        check_admin(@user)
         if @user.persisted?
             sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
             set_flash_message(:notice, :success, :kind => "Google OAuth") if is_navigational_format?
@@ -32,6 +16,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def check_admin(user)
+    supervisor_list = Rails.configuration.api["supervisors"]
+    admin_list = Rails.configuration.api["admins"]
+
+    if admin_list.include? user.email
+        user.update_attribute :admin, true
+        user.update_attribute :supervisor, true
+    elsif supervisor_list.include? user.email
+        user.update_attribute :supervisor, true
+    else
+        user.update_attribute :admin, false
+        user.update_attribute :supervisor, false
+    end
+    user.save
+  end
+  def request_from_env
+    User.from_omniauth(request.env['omniauth.auth'])
+  end
   def failure
     redirect_to root_path
   end
