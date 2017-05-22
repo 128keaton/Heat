e3bac07fc1c42c9397628220a8fd3691eda9c262
Keaton Burleson <keaton.burleson@me.com>
2017-05-22 11:45:21 -0500

    Ugh more JS refactoring

--- app/assets/javascripts/receive.js
+++ app/assets/javascripts/receive.js
@@ -1,23 +1,31 @@
-$(document).ready(function () {
-  $('#submit').attr('disabled', 'disabled');
-  $('#submit-choose').attr('disabled', 'disabled');
+$(document).ready(function() {
+    $('#submit').attr('disabled', 'disabled');
+    $('#submit-choose').attr('disabled', 'disabled');
 
-  $('#machine_serial_number').bind('input', function () {
-    if ($('#machine_serial_number').val().length == 7 && $('#machine_pallet_id').val().length > 0) {
-      return $('#submit').removeAttr('disabled');
-    } else {
-      return $('#submit').attr('disabled', 'disabled');
-    }
-  });
+    $('#machine_serial_number').bind('input', function() {
+        check_for_submit();
+    });
 
-  $('#machine_pallet_id').bind('input', function () {
-    if ($('#machine_pallet_id').val().length > 0) {
-      return $('#submit-choose').removeAttr('disabled');
-    } else {
-      return $('#submit-choose').attr('disabled', 'disabled');
-    }
-  });
+    $('#machine_pallet_id').bind('input', function() {
+        check_for_submit_choose();
+    });
 
-  $("#machine_serial_number").focus();
+    $("#machine_serial_number").focus();
 
 });
+
+function check_for_submit_choose() {
+    if ($('#machine_pallet_id').val().length > 0) {
+        return $('#submit-choose').removeAttr('disabled');
+    } else {
+        return $('#submit-choose').attr('disabled', 'disabled');
+    }
+}
+
+function check_for_submit() {
+    if ($('#machine_serial_number').val().length === 7 && $('#machine_pallet_id').val()) {
+        return $('#submit').removeAttr('disabled');
+    } else {
+        return $('#submit').attr('disabled', 'disabled');
+    }
+}
