76ada91661cd89240cf2bb4d10d069efc2421f8f
root <root@er2memphis.er2.com>
2017-05-31 12:46:12 -0500

    Added migration for Teacher OUs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523200231) do
+ActiveRecord::Schema.define(version: 20170531174233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20170523200231) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "location"
+    t.string   "ou_string"
+    t.string   "teacher_ou"
   end
 
   create_table "users", force: :cascade do |t|
