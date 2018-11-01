0a8e6183265d2217c347a03152e7d48d18e0f423
Keaton Burleson <keaton.burleson@me.com>
2018-11-01 14:59:37 -0500

    Adds matcher support for same model numbers, different serial

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,4 @@
 class Model < ApplicationRecord
-  validates :number, uniqueness: true, presence: true
+  validates :number, presence: true
   validates :first_match, presence: true, uniqueness: true
 end
